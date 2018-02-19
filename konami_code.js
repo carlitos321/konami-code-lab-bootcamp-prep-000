04f1e1fd5fd0f47745fc856b0066d1743b7bc4c0
carlitos321 <ca.cabrera321@gmail.com>
2018-02-19 14:54:40 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
+const body = document.querySelector('body')
+
 function init() {
+<<<<<<< HEAD
   let index = 0;
 
   document.body.addEventListener('keydown', function(e) {
@@ -18,4 +21,15 @@ function init() {
       index = 0;
     }
   });
+=======
+  body.addEventListener('keydown', function(e) {
+    // if user types the code in order, then send x alert
+    const key = parseInt(e.detail || e.which);
+
+    for (let i = 0, l = code.length; i < l; i++) {
+      console.log(code[i])
+    }
+  } )
+
+>>>>>>> 7fc23a676c99d27b4d4fd599706c2a1d29be4e18
 }
