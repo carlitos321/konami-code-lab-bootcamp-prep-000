432325cfe18ae9066e88960348e19c7d9e23216f
carlitos321 <ca.cabrera321@gmail.com>
2018-02-19 14:54:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -21,15 +21,3 @@ function init() {
       index = 0;
     }
   });
-=======
-  body.addEventListener('keydown', function(e) {
-    // if user types the code in order, then send x alert
-    const key = parseInt(e.detail || e.which);
-
-    for (let i = 0, l = code.length; i < l; i++) {
-      console.log(code[i])
-    }
-  } )
-
->>>>>>> 7fc23a676c99d27b4d4fd599706c2a1d29be4e18
-}
