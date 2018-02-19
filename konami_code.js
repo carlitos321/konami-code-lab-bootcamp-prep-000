96b066198a761d157aae28d902845fa8a7ea1555
carlitos321 <ca.cabrera321@gmail.com>
2018-02-19 14:56:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -21,13 +21,13 @@ function init() {
     }
   });
 
-  body.addEventListener('keydown', function(e) {
-    // if user types the code in order, then send x alert
-    const key = parseInt(e.detail || e.which);
-
-    for (let i = 0, l = code.length; i < l; i++) {
-      console.log(code[i])
-    }
-  } )
+  // body.addEventListener('keydown', function(e) {
+  //   // if user types the code in order, then send x alert
+  //   const key = parseInt(e.detail || e.which);
+  // 
+  //   for (let i = 0, l = code.length; i < l; i++) {
+  //     console.log(code[i])
+  //   }
+  // } )
 
 }
